4dcfa44c0cff7eb924fdc615bfbbca27e5b9df7b
John T Skarbek <jtslear@gmail.com>
2016-02-22 23:05:02 -0500

    Able to define struct now
    * unable to test updating a color
    * I don't understand how the agent is supposed to do according to docs
    * I think i'm clashing between my defined function and the agent is holding me to that function?

--- lib/elixirlight/colorchanger.ex
+++ lib/elixirlight/colorchanger.ex
@@ -6,16 +6,18 @@ defmodule Elixirlight.Colorchanger do
   Using RGB here cuz that is what our device uses
   """
   def start_link do
-    color = %ColorUtils.RGB{red: 255, green: 255, blue: 255}
-    Agent.start_link(fn -> color end)
+    Agent.start_link(fn -> %ColorUtils.RGB{} end)
   end
 
   @doc """
   Get the current color from the agent
   We return Hex values for color as that is how our web app manages it
   """
-  def get(agent, color) do
-    Agent.get(agent, %ColorUtils.rgb_to_hex(color))
+  def get(agent) do
+    Agent.get(agent, &ColorUtils.rgb_to_hex(&1))
   end
 
+  def define_color(agent, color) do
+    Agent.update(agent, &ColorUtils.hex_to_rgb(&1))
+  end
 end
--- test/colorchanger/colorchanger_test.exs
+++ test/colorchanger/colorchanger_test.exs
@@ -3,6 +3,12 @@ defmodule Elixirlight.ColorchangerTest do
 
   test "gets initial color" do
     {:ok, color} = Elixirlight.Colorchanger.start_link
-    assert Elixirlight.Colorchanger.get(color) == "#FFFFFF"
+    assert Elixirlight.Colorchanger.get(color) == "#"
+  end
+
+  test "set color to white" do
+    {:ok, color} = Elixirlight.Colorchanger.start_link
+    Elixirlight.Colorchanger.define_color(color, "#FFFFFF")
+    assert Elixirlight.Colorchanger.get(color) == "#FFFFF"
   end
 end
